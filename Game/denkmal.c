52333334d4cf02419925c38447335bccc32ea102
Nosferatu <Cvalenta@freenet.de>
2014-01-23 15:33:52 +0100

    Random Field und Stone eingebaut

--- Game/denkmal.c
+++ Game/denkmal.c
@@ -1,4 +1,5 @@
 #include "denkmal.h"
+#include <time.h>
 
 char spielzug[10];
 extern struct shmInfos *shmPtr;
@@ -111,8 +112,13 @@ void thinkbetter() {
 }
 //gibt freies Feld zurück
 int randomField(int felder[]) {
-	int index=0;
-	int len = shmPtr->hoehe * shmPtr->breite;
+	srand(time(NULL));
+	int len = shmPtr->hoehe * shmPtr->breite; 
+	// Random Int von 0 bis Groesse des Spielfeldes
+	int index= rand() % len;
+	int gleicherIndex = index;
+	//30 Versuche um Random ein freies Feld zu suchen.
+	int i = 30;
 	//int shmPtr_Sf[16] = { 9, 7, -1, 8, 3, -1, 2, -1, 15, -1, 6, 4, -1, 11, -1, 10 };
 
 	
@@ -121,20 +127,38 @@ int randomField(int felder[]) {
 		if(felder[index] == 0 ) {
 			break;
 		}
-		index++;
-	} while(index < len);
+		index = rand() % len;
+		if(index = gleicherIndex){
+		index = rand() % len;
+		gleicherIndex = index;
+		}
+		i--;
+	} while(i>0);
 	log_printf(LOG_DEBUG,"Feld: %i\n",index);
 	return index;
 }
 // gibt möglichen Spielstein, oder -1 zurück
 int randomStone(int stones[]) {
 	//freien Spielstein suchen und speichern
-	int nextStein=0;
-	while (nextStein < 16) {
-		if (stones[nextStein] ==0){
+	//int nextStein=0;
+	srand(time(NULL));
+	//waehlt nextStein random aus 0 und 15
+	int nextStein = rand() % 16;
+	int gleicherStein = nextStein;
+	// 30 Versuche einen naechsten Stein zu finden sollte ja reichen 
+	int i = 30;
+while(i > 0){	
+	if (stones[nextStein] ==0){
 			return nextStein;
 		}
-		nextStein++;
+		else{
+		nextStein = rand() % 16;
+		if(nextStein = gleicherStein){
+		nextStein = rand() % 16;
+		gleicherStein = nextStein;
+		}
+	}
+	i--;
 	}
 	log_printf(LOG_DEBUG,"Stein: %i\n",nextStein);
 	return -1;
